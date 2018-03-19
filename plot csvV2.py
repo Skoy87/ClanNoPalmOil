7390356138771718439107e438f0f20dc92719b1
Skoy87 <alessio.colizza@gmail.com>
2018-03-19 20:41:01 +0000

    Updated plot tools more data on the csv

---
+++ plot csvV2.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+font = {'family' : 'arial',
+        'weight' : 'light',
+        'size'   : 8}
+
+plt.rc('font', **font)
+pd.options.display.width=None
+dfToPlot=pd.read_csv(filepath_or_buffer='clan.csv')
+# print(dfToPlot)
+pivotedTrophies= dfToPlot.pivot(index='date', columns='name', values='trophies')
+#pivotedTrophies.plot(legend(loc=7, fontsize=8))
+print(pivotedTrophies.plot())
+
+
+
+
+# groupTag=dfToPlot.groupby('tag')
+# trop = groupTag['donations', 'donationsReceived']
+#
+#
+# trop.plot(title='name')
+
+# for name, trophies in dfToPlot.groupby('tag'):
+#     trophies.plot(x='trophies', y='date', title='title')
+
+#groupDF = dfToPlot.groupby(['tag', 'name']).count()
+#print(groupDF)
+
+#print(dfToPlot.plot())
+plt.show()
