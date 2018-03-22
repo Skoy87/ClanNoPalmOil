127e3aed4875f5476edb9b3bb5670f783443b101
Anna Russo <anna.russo@qb0054-mac.lan>
2018-03-22 15:51:57 +0000

    pipeline for plots of the stats

--- Rversion/plots_for_Ale.R
+++ Rversion/plots_for_Ale.R
@@ -1,23 +1,51 @@
-rm(list=ls())
-library(data.table)
-library(dplyr)
-library(ggplot2)
-data <- fread("../clan.csv")
-data$time <- unlist(lapply(strsplit(data$date," "), function(x) x[2]))
-data$date <- unlist(lapply(strsplit(data$date," "), function(x) x[1]))
-data[grep("2018-03-19", date)]$date <- "19/03/2018"
-data[grep("2018-03-20", date)]$date <- "20/03/2018"
-data$date <- as.Date(data$date,format = "%d/%m/%Y")
+#INSTRUCTIONS:
+# 1. source this file
+# 2. run the following lines:
+#    OUTPUT_DIR = 'plots/'
+#    PATH_TO_CLAN_CSV = '../clan_cleaned.csv'
+# 3. run plots_stats <- pipeline_plots(PATH_TO_CLAN_CSV=PATH_TO_CLAN_CSV,OUTPUT_DIR=OUTPUT_DIR)
 
-data$date_time <- as.POSIXct(with(data, paste0(date," ",time)))
 
-data <- as.data.table(data %>% select(-one_of(c("time","date"))))
-data_melted <- melt(data,id.vars = c("tag","name","date_time"))
+#OUTPUT_DIR = 'plots/'
+#PATH_TO_CLAN_CSV = '../clan_cleaned.csv'
 
-plot_trophies_by_user <- ggplot(data_melted[variable=='trophies']) + geom_line(aes(x=date_time,y=value)) +
-  theme_bw()+ theme(axis.text.x = element_text(angle = 90))+
-  scale_x_datetime() + xlab("Day - time") + ylab("Trophies")+facet_wrap(~name)
+pipeline_plots <- function(PATH_TO_CLAN_CSV, OUTPUT_DIR){
+  load_pkgs()
+  if(!dir.exists(OUTPUT_DIR)){dir.create(path = OUTPUT_DIR)}
+  plots_stats <- plot_stats_clan(path_to_clan_csv=PATH_TO_CLAN_CSV,
+                                 output_dir = OUTPUT_DIR)
+  return(plots_stats)
+}
 
-plot_donations_by_user <- ggplot(data_melted[variable %in% c('donations','donationsReceived')]) + 
-  geom_line(aes(x=date_time,y=value, colour=variable)) + theme_bw()+ theme(axis.text.x = element_text(angle = 90, hjust=1))+
-  scale_x_datetime() + xlab("Day - time") + ylab("Donations")+facet_wrap(~name)
+load_pkgs <- function(){
+  library(data.table)
+  library(dplyr)
+  library(ggplot2)
+}
+
+plot_stats_clan <- function(path_to_clan_csv, output_dir){
+  
+  data <- fread(path_to_clan_csv)
+  data$date <- as.POSIXct(data$date)
+  data_melted <- melt(data,id.vars = c("tag","name","date"))
+  
+  plot_trophies_by_user <- ggplot(data_melted[variable=='trophies']) + geom_line(aes(x=date,y=value)) +
+    theme_bw()+ theme(axis.text.x = element_text(angle = 90))+
+    scale_x_datetime() + xlab("Day - time") + ylab("Trophies")+facet_wrap(~name)
+  
+  plot_donations_by_user <- ggplot(data_melted[variable %in% c('donations','donationsReceived')]) + 
+    geom_line(aes(x=date,y=value, colour=variable)) + theme_bw()+ theme(axis.text.x = element_text(angle = 90, hjust=1))+
+    scale_x_datetime() + xlab("Day - time") + ylab("Donations")+facet_wrap(~name)
+  
+  list_plots <- list()
+  list_plots$plot_trophies_by_user <- plot_trophies_by_user
+  list_plots$plot_donations_by_user <- plot_donations_by_user
+  
+  trophies_plot_name = paste0(output_dir,"/trophies_plot_",Sys.Date(),".pdf")
+  donations_plot_name = paste0(output_dir,"/donations_plot_",Sys.Date(),".pdf")
+  
+  ggsave(plot = plot_trophies_by_user,filename = trophies_plot_name, width = 30, units = "cm")
+  ggsave(plot = plot_donations_by_user,filename = donations_plot_name, width = 30, units = "cm")
+  
+  return(list_plots)
+}
